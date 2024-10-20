03f4c41422e403c34605ac3ba8f4c7c3b6303dde
Dereksit0 <128322660+Dereksit0@users.noreply.github.com>
2024-10-20 17:02:11 -0600

    first commit
    20/10/2024

---
+++ 2/src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+    pc: `Esta es mi PC. Ultimamente me dedico a aprender JavaScript con el framework de React.
+     He creado algunos proyectos en React como paginas web, videojuegos, etc. En general, soy un apasionado de la tecnologia.
+      Aqui puedes ver algunos de mis proyectos <a href="https://github.com/Dereksit0" target="_blank">Github</a>!`,
+    "cs-degree": `Este es mi titulo de Ingenieria de Software en la UMAD. Lo puse en la pared porque estoy muy orgulloso de haberlo conseguido. Fue un proceso muy largo pero aprendi las bases de mis conocimientos actuales.`,
+    "sofa-table": `Este es mi sofa. Donde me gusta relajarme y ver algunos videos en YouTube.
+    Tambien hago algunos tutoriales de proyectos como este. ¡Suscribete aqui <a href="https://www.youtube.com/@derekramirezenriquez475" target="_blank">mi canal</a>! (Si te gusta mi contenido)`,
+    tv: `Esta es mi television. Donde me gusta ver a mis creadores de contenido favoritos como:
+     <a href="https://www.youtube.com/@LaPensionCF" target="_blank">LaPension</a>, <a href="https://www.youtube.com/@NateGentile7" target="_blank">NateGentile</a>,
+    <a href="https://www.youtube.com/@midulive" target="_blank">Midudev</a> (a veces) y <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>!`,
+    bed: `Esta es mi cama. Grandes ideas me llegan cuando estoy acostado. Cuando se me ocurre una idea, generalmente tengo que anotarla o no podre dormir porque me consume la energia mental.`,
+    resume: `Este es mi escritorio y en el esta mi curriculum. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">¿Quieres verlo?</a>
+    ¡Contactame en ramirezderek49gmail.com si tienes alguna oferta de trabajo interesante!`,
+    projects: `Informacion sobre este portafolio: esta hecho con la libreria Kaboom.js, que es una libreria para crear videojuegos en JavaScript.
+    El texto esta renderizado con HTML/CSS. Asi que el cuadro de texto que estas leyendo no esta renderizado dentro del canvas. Aprende mas sobre como usar
+    Kaboom.js viendo algunos de mis tutoriales <a href="https://www.youtube.com/@derekramirezenriquez475" target="_blank">aqui</a>.`,
+    library: `Tengo muchos libros de programacion en mis estantes. 
+    Probablemente solo he leido uno de ellos. ¿Quien mas compra compulsivamente libros tecnicos sin terminar de leerlos?`,
+    exit: `Si quieres salir del portafolio de CV Derek Ramirez, solo cierra la pestana.`,
+  };
+  
